efb27b45f7615f51ff90cfe71710b1e6526b0f45
AabdirahmanShafi <117932483+AabdirahmanShafi@users.noreply.github.com>
2022-11-12 12:15:12 +0300

    Add files via upload

---
+++ user profile.cpp
@@ -0,0 +1,28 @@
+#include <iostream> 
+#include <string> 
+using namespace std; 
+
+int main() 
+{ 
+    string firstname, lastname, fullname, favoritemeal, favoritemovie; 
+    cout<<"Enter first name:";
+    cin>>firstname;
+    cout<<"\nEnter last name:";
+    cin>>lastname;
+    cout<< "\nFull name is: "<< firstname << " "<< lastname <<endl;
+    cout<<"\nEnter favorite meal ";
+    cin>>favoritemeal;
+
+    cout<<"\nEnter favorite movie ";
+    cin>>favoritemovie;
+    
+
+
+    
+
+    cout<<endl;
+
+
+    
+
+}
